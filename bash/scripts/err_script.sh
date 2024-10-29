33f1c115234b0feedaa58c7d29bb3e3358a7091b
romanITst <rkurtasov@gmail.com>
2024-10-29 09:47:52 +0000

    variable name updated

--- bash/scripts/err_script.sh
+++ bash/scripts/err_script.sh
@@ -3,12 +3,12 @@
 # Description: This script will remove the file, if it finds the word "error" in it.
 
 
-read -p "Enter the path to file: " ptf
+read -p "Enter the path to file: " path_to_file
 
 #if sed -n /error/p $ptf
-if grep -w -o error $ptf 
+if grep -w -o error $path_to_file
 then
-	rm -i -v $ptf
+	rm -i -v $path_to_file
 else
 	echo "Error messages wasn't found. Terminate..."
 fi
