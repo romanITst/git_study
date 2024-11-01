0ca9bd6a7b2f869d259069cc57184240a80ca490
romanITst <rkurtasov@gmail.com>
2024-11-01 13:04:56 +0000

    task added

--- python/script.py
+++ python/script.py
@@ -1,6 +1,8 @@
 #! /usr/bin/env python3
 
+
 #task description: написать скрипт заменяющий одну букву на другую в слове
+
 #word1 = str(input("Enter the word: "))
 #letter1 = str(input("Enter the letter you want to change in the word: "))
 #letter2 = str(input(f"Enter the letter you want to replace with the {letter1} in the word: "))
@@ -8,8 +10,8 @@
 #print(f"The result is: {word2}")
 
 
-
 #task description: написать генератор пароля
+
 #import random
 #
 #password = "abcdefghijklopqrstuvwxyz!@#$%^&*()_+".join("ABCDEFGHIJKLOPQRSTUVWXYZ123215498712936789213")
@@ -20,10 +22,20 @@
 #print(f"Your password is: {a} \nSave it in safe place and dont show anybody!")
 
 
-#task description: отсортировать данный массив из чисел
-numbers = [1, 3, 5, 2, 0, 75, 71, 69, 68, 34, 23, 99]
-print(sorted(numbers))
-# + вывести числа меньше определённого
-for num in numbers:
-    if num < 5:
-        print(num)
+##task description: отсортировать данный массив из чисел
+
+#numbers = [1, 3, 5, 2, 0, 75, 71, 69, 68, 34, 23, 99]
+#print(sorted(numbers))
+## + вывести числа меньше определённого
+#for num in numbers:
+#    if num < 5:
+#        print(num)
+
+
+#task description: посчитать сумму цифр двузначного числа введённого с клавиатуры
+
+twod_number = input("Enter the two-digit number: ")
+digits_summ  = 0
+for num in twod_number:
+    digits_summ += int(num)
+print(digits_summ)
