a3c810c4dd9dd505787f366e82ceb70742daeefc
romanITst <rkurtasov@gmail.com>
2024-11-01 10:40:06 +0000

    tasks descriptions added. new task added

--- python/script.py
+++ python/script.py
@@ -1,11 +1,15 @@
 #! /usr/bin/env python3
 
-word1 = str(input("Enter the word: "))
-letter1 = str(input("Enter the letter you want to change in the word: "))
-letter2 = str(input(f"Enter the letter you want to replace with the {letter1} in the word: "))
-word2 = word1.replace(f"{letter1}", f"{letter2}")
-print(f"The result is: {word2}")
+#task description: написать скрипт заменяющий одну букву на другую в слове
+#word1 = str(input("Enter the word: "))
+#letter1 = str(input("Enter the letter you want to change in the word: "))
+#letter2 = str(input(f"Enter the letter you want to replace with the {letter1} in the word: "))
+#word2 = word1.replace(f"{letter1}", f"{letter2}")
+#print(f"The result is: {word2}")
 
+
+
+#task description: написать генератор пароля
 #import random
 #
 #password = "abcdefghijklopqrstuvwxyz!@#$%^&*()_+".join("ABCDEFGHIJKLOPQRSTUVWXYZ123215498712936789213")
@@ -14,3 +18,8 @@ print(f"The result is: {word2}")
 #a = "".join(random.sample(password,password_length))
 #
 #print(f"Your password is: {a} \nSave it in safe place and dont show anybody!")
+
+
+#task description: отсортировать данный массив из чисел
+a = [1, 3, 5, 2, 0, 75, 71, 69, 68, 34, 23, 99]
+print(sorted(a))
