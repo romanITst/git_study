a79671d6629a2fb3eb413b09e3b50bcac327a005
romanITst <rkurtasov@gmail.com>
2024-11-01 10:51:07 +0000

    new task added

--- python/script.py
+++ python/script.py
@@ -21,5 +21,9 @@
 
 
 #task description: отсортировать данный массив из чисел
-a = [1, 3, 5, 2, 0, 75, 71, 69, 68, 34, 23, 99]
-print(sorted(a))
+numbers = [1, 3, 5, 2, 0, 75, 71, 69, 68, 34, 23, 99]
+print(sorted(numbers))
+# + вывести числа меньше определённого
+for num in numbers:
+    if num < 5:
+        print(num)
