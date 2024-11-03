e3345db027adf7c3f4a733e5e1ad7fd4acb99950
romanITst <rkurtasov@gmail.com>
2024-11-03 13:35:28 +0000

    Decipher script wroted.d

--- python/decripto.py
+++ python/decripto.py
@@ -1,10 +1,23 @@
 #! /usr/bin/env python3
 
 
-#importing the crypto lib
+from cryptography.fernet import Fernet
+path_to_key = str(input("Enter the path to cryptography key: "))
+with open(f"{path_to_key}", "rb") as path_to_key:
+    key = path_to_key.read()
+
+decipher = Fernet(key) #decipher function
 
 #path to encrypted_file
+encrypted_file = str(input("Enter the path to file you want to decrypt: "))
 
 #reading encrypted file 
+with open(f"{encrypted_file}", "rb") as encrypted_file:
+    encrypted_data = encrypted_file.read()
 
+decrypted_data = decipher.decrypt(encrypted_data)
+
+decrypted_file = str(input("Enter the path you want to save the decrypted file: "))
 #writing decripted file
+with open(f"{decrypted_file}", "wb") as decrypted_file:
+    decrypted_file.write(decrypted_data)
