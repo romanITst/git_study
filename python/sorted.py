9accac534f57ae0a04e0bfe21e114d28b818226b
romanITst <rkurtasov@gmail.com>
2024-11-03 11:55:07 +0000

    Sorted function have been added to 'lt' numbers. Spell changes.

--- python/sorted.py
+++ python/sorted.py
@@ -1,10 +1,14 @@
 #! /usr/bin/env python3
 
-##task description: отсортировать данный массив из чисел
+# Task description: отсортировать данный массив из чисел
 
 numbers = [1, 3, 5, 2, 0, 75, 71, 69, 68, 34, 23, 99]
 print(sorted(numbers))
-# + вывести числа меньше определённого
+
+# +вывести числа меньше определённого (допустим 5)
+less_numbers = []
 for num in numbers:
     if num < 5:
-        print(num)
+        less_numbers.append(num)
+    
+print(sorted(less_numbers))
