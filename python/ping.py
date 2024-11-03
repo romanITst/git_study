3135eaab7b91f5fe6d71cf68e997acec46579ad5
romanITst <rkurtasov@gmail.com>
2024-11-03 14:36:05 +0000

    Script wroted

--- python/ping.py
+++ python/ping.py
@@ -1,1 +1,8 @@
 #! /usr/bin/env python3
+
+import os
+
+ip_addr = str(input("Enter the address to ping: "))
+count = str(input("Enter the times of ping: "))
+
+print(os.popen(f"ping {ip_addr} -c {count}").read())
